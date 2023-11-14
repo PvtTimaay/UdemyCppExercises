45ab3fef3c8f9da733342386c4d86265a926f6fc
PvtTimaay <trip_tripper@gmx.net>
2023-11-14 20:51:05 +0100

    message

--- 03_ArrayPointer/AlgoLib/Exercise/lib.cc
+++ 03_ArrayPointer/AlgoLib/Exercise/lib.cc
@@ -2,27 +2,103 @@
 #include <cstdint>
 
 #include "lib.h"
+//              {3, 1, 4, 1, 5, 9, 2, 6};
 
-bool all_of(const std::int32_t *array,
-            const std::size_t length,
-            const std::int32_t value)
+bool all_of(const std::int32_t *array, const std::size_t length, const std::int32_t value)
 {
+    std::size_t Rainer = 0;
+    if (array == nullptr || length <= 0)
+    {
+        return false;
+    }
+    for (size_t i = 0; i < length; i++)
+    {
+
+        if (array[i] == value)
+        {
+            Rainer++;
+        }
+    }
+    if (Rainer == length)
+    {
+        return true;
+    }
+    else
+        return false;
 }
 
-bool any_of(const std::int32_t *array,
-            const std::size_t length,
-            const std::int32_t value)
+bool any_of(const std::int32_t *array, const std::size_t length, const std::int32_t value)
 {
+    if (array == nullptr || length <= 0)
+    {
+        return false;
+    }
+    std::size_t Rainer = 0;
+    if (array == nullptr || length <= 0)
+    {
+        return false;
+    }
+    for (size_t i = 0; i < length; i++)
+    {
+
+        if (array[i] == value)
+        {
+            Rainer++;
+        }
+    }
+    if (Rainer >= 1)
+    {
+        return true;
+    }
+    else
+        return false;
 }
 
-bool none_of(const std::int32_t *array,
-             const std::size_t length,
-             const std::int32_t value)
+bool none_of(const std::int32_t *array, const std::size_t length, const std::int32_t value)
 {
+    if (array == nullptr || length <= 0)
+    {
+        return false;
+    }
+    std::size_t Rainer = 0;
+    if (array == nullptr || length <= 0)
+    {
+        return false;
+    }
+    for (size_t i = 0; i < length; i++)
+    {
+
+        if (array[i] == value)
+        {
+            Rainer++;
+        }
+    }
+    if (Rainer == 0)
+    {
+        return true;
+    }
+    else
+        return false;
 }
 
-std::size_t count(const std::int32_t *array,
-                  const std::size_t length,
-                  const std::int32_t value)
+std::size_t count(const std::int32_t *array, const std::size_t length, const std::int32_t value)
 {
+    if (array == nullptr || length <= 0)
+    {
+        return 0;
+    }
+    std::size_t Rainer = 0;
+    if (array == nullptr || length <= 0)
+    {
+        return false;
+    }
+    for (size_t i = 0; i < length; i++)
+    {
+
+        if (array[i] == value)
+        {
+            Rainer++;
+        }
+    }
+    return Rainer;
 }
