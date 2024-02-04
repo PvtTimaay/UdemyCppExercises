17759c15a1f06ef94cac1a3a8233d03101734935
PvtTimaay <trip_tripper@gmx.net>
2024-02-04 16:22:09 +0100

    AriseWordCountPair

--- 06_String/SplitWords/Exercise/Lib.h
+++ 06_String/SplitWords/Exercise/Lib.h
@@ -4,11 +4,11 @@
 #include <string_view>
 #include <vector>
 
+using WordCountPair = std::pair<std::string, std::size_t>;
+
 void clean_text(std::string &text);
 
-void replace_all(std::string &text,
-                 std::string_view what,
-                 std::string_view with);
+void replace_all(std::string &text, std::string_view what, std::string_view with);
 
 template <typename T>
 void split_text(const std::string &text, char delimiter, T result);
