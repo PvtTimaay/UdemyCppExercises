3a6920786bb09d62b3ba8e4f24ed0358b2b67813
PvtTimaay <trip_tripper@gmx.net>
2024-02-05 22:41:06 +0100

    mssge

--- 06_String/SplitWords/Exercise/Lib.cc
+++ 06_String/SplitWords/Exercise/Lib.cc
@@ -1,24 +1,25 @@
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <string_view>
-#include <vector>
 #include <utility>
-#include <sstream>
+#include <vector>
 
 #include "Lib.h"
 
 void clean_text(std::string &text)
 {
-
+    size_t position = 0; // Startposition für die Suche
+    while ((position = text.find(",", position)) != std::string::npos)
+    {
+        text.erase(position, 1); // Entfernt 1 Zeichen an der Position 'position'
+    }
 }
 
-void replace_all(std::string &text,
-                 std::string_view what,
-                 std::string_view with)
+void replace_all(std::string &text, std::string_view what, std::string_view with)
 {
-
 }
 
 std::vector<std::string> split_text(const std::string &text, char delimiter)
 {
-
 }
