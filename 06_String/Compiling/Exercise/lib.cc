f3f73bd37be2ce0416db7a42330eb0832a927cc8
PvtTimaay <trip_tripper@gmx.net>
2024-02-07 22:50:18 +0100

    isSourceGetSource

--- 06_String/Compiling/Exercise/lib.cc
+++ 06_String/Compiling/Exercise/lib.cc
@@ -1,16 +1,27 @@
 #include <vector>
 #include <filesystem>
+#include <iostream>
 
 #include "lib.h"
 
 bool is_source_file(const fs::path &file)
 {
+    if (file.extension() == ".cc" || ".cpp" || ".h" || ".hpp")
+    {
+        return true;
+    }
 
 }
 
 std::vector<fs::path> get_source_files_in_dir(const fs::path &dir)
 {
-
+    if (is_source_file(dir))
+    {
+        std::vector<fs::path> tempPathVec;
+        tempPathVec.push_back(dir);
+        return tempPathVec;
+    }
+    std::cout << "no C++ source file" << std::endl;
 }
 
 void compile_file(fs::path source_file)
