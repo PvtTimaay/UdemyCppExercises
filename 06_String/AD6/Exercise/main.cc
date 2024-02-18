21617e31dfb6c1c6a3a4350e8eb88574afed9d5a
PvtTimaay <trip_tripper@gmx.net>
2024-02-18 13:35:21 +0100

    mssge

--- 06_String/AD6/Exercise/main.cc
+++ 06_String/AD6/Exercise/main.cc
@@ -9,8 +9,25 @@
 
 #include "utils.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
+    std::filesystem::path data_filepath{};
+    if (argc < 2)
+    {
+        data_filepath /= std::filesystem::current_path();
+        data_filepath /= "data";
+    }
+    else
+    {
+        const auto data_path_str = std::string(argv[1]);
+        data_filepath = std::filesystem::path(data_path_str);
+    }
+
+    std::filesystem::path ego_filepath = data_filepath;
+    ego_filepath /= "ego_data.json";
+    std::filesystem::path vehicle_filepath = data_filepath;
+    vehicle_filepath /= "vehicle_data.json";
+
     auto ego_vehicle = VehicleType{};
     auto vehicles = NeighborVehiclesType{};
 
@@ -31,14 +48,11 @@ int main()
         print_scene(ego_vehicle, vehicles);
         compute_future_state(ego_vehicle, vehicles, 0.100F);
 
-        const auto ego_lane_vehicles =
-            get_vehicle_array(ego_vehicle.lane, vehicles);
+        const auto ego_lane_vehicles = get_vehicle_array(ego_vehicle.lane, vehicles);
         longitudinal_control(ego_lane_vehicles[0], ego_vehicle);
 
-        const auto lane_change_request =
-            get_lane_change_request(ego_vehicle, vehicles);
-        const auto lane_change_executed =
-            lateral_control(lane_change_request, ego_vehicle);
+        const auto lane_change_request = get_lane_change_request(ego_vehicle, vehicles);
+        const auto lane_change_executed = lateral_control(lane_change_request, ego_vehicle);
 
         if (lane_change_executed)
         {
--- 06_String/AD7/Exercise/DataLoader.cc
+++ 06_String/AD7/Exercise/DataLoader.cc
@@ -6,21 +6,43 @@
 #include "DataLoaderConstants.hpp"
 #include "DataLoaderTypes.hpp"
 
-static VehiclesData vehicles_data = std::vector<std::vector<VehicleType>>(
-    NUM_VEHICLES,
-    std::vector<VehicleType>(NUM_ITERATIONS));
+static VehiclesData vehicles_data =
+    std::vector<std::vector<VehicleType>>(NUM_VEHICLES, std::vector<VehicleType>(NUM_ITERATIONS));
 
 void init_ego_vehicle(std::string_view filepath, VehicleType &ego_vehicle)
 {
+    std::ifstream ifs(filepath.data());
+    nlohmann::json parsed_data = nlohmann::json::parse(ifs);
 
+    ego_vehicle.id = EGO_VEHICLE_ID;
+    ego_vehicle.lane = parsed_data["Lane"];
+    ego_vehicle.speed_mps = parsed_data["Speed"];
+    ego_vehicle.distance_m = 0.0f;
 }
 
 void init_vehicles(std::string_view filepath, NeighborVehiclesType &vehicles)
 {
+    std::ifstream ifs(filepath.data());
+    nlohmann::json parsed_data = nlohmann::json::parse(ifs);
 
+    for (std::size_t vehicle_index = 0; vehicle_index < NUM_VEHICLES; vehicle_index++)
+    {
+        const nlohmann::json &vehicle_data = parsed_data[std::to_string(vehicle_index)];
+        const std::int32_t id = static_cast<std::int32_t>(vehicle_index);
+        const LaneAssociationType lane = static_cast<LaneAssociationType>(vehicle_data["Lane"]);
+        const float distance_m = static_cast<float>(vehicle_data["Distance"]);
+
+        for (std::size_t it = 0; it < NUM_ITERATIONS; it++)
+        {
+            const float speed_mps = static_cast<float>(vehicle_data["Speed"][it]);
+            vehicles_data[vehicle_index][it] = {.id = id,
+                                                .lane = lane,
+                                                .speed_mps = speed_mps,
+                                                .distance_m = distance_m};
+        }
+    }
 }
 
 void load_cycle(const std::uint32_t cycle, NeighborVehiclesType &vehicles)
 {
-
 }
