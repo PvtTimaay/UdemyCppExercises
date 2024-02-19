d8d41227c58600c0a584c7d51255d8e0b41f487f
PvtTimaay <trip_tripper@gmx.net>
2024-02-19 20:41:54 +0100

    mssge

--- 06_String/AD7/Exercise/DataLoader.cc
+++ 06_String/AD7/Exercise/DataLoader.cc
@@ -41,8 +41,21 @@ void init_vehicles(std::string_view filepath, NeighborVehiclesType &vehicles)
                                                 .distance_m = distance_m};
         }
     }
+    // Zyklen vorladen
+    vehicles.vehicles_left_lane[0] = vehicles_data[0][0];
+    vehicles.vehicles_left_lane[1] = vehicles_data[1][0];
+    vehicles.vehicles_center_lane[0] = vehicles_data[2][0];
+    vehicles.vehicles_center_lane[1] = vehicles_data[3][0];
+    vehicles.vehicles_right_lane[0] = vehicles_data[4][0];
+    vehicles.vehicles_right_lane[1] = vehicles_data[5][0];
 }
 
 void load_cycle(const std::uint32_t cycle, NeighborVehiclesType &vehicles)
 {
+    vehicles.vehicles_left_lane[0].speed_mps     =    vehicles_data[0][cycle].speed_mps;
+    vehicles.vehicles_left_lane[1].speed_mps     =    vehicles_data[1][cycle].speed_mps;
+    vehicles.vehicles_center_lane[0].speed_mps   =    vehicles_data[2][cycle].speed_mps;
+    vehicles.vehicles_center_lane[1].speed_mps   =    vehicles_data[3][cycle].speed_mps;
+    vehicles.vehicles_right_lane[0].speed_mps    =    vehicles_data[4][cycle].speed_mps;
+    vehicles.vehicles_right_lane[1].speed_mps    =    vehicles_data[5][cycle].speed_mps;
 }
