e048cc4994a9789e2c78954e6455c5bc430c8311
PvtTimaay <trip_tripper@gmx.net>
2024-02-24 16:47:59 +0100

    mssge

--- 07_Container/WordCount/Exercise/Lib.cc
+++ 07_Container/WordCount/Exercise/Lib.cc
@@ -1,4 +1,6 @@
+#include <algorithm>
 #include <filesystem>
+#include <iostream>
 #include <map>
 #include <string>
 #include <string_view>
@@ -16,9 +18,7 @@ void clean_text(std::string &text)
     replace_all(text, "\t", "");
 }
 
-void replace_all(std::string &text,
-                 std::string_view what,
-                 std::string_view with)
+void replace_all(std::string &text, std::string_view what, std::string_view with)
 {
     for (std::size_t pos = 0; pos != std::string::npos; pos += with.length())
     {
@@ -46,16 +46,36 @@ WordVector split_text(const std::string &text, char delimiter)
     return words;
 }
 
-CountedWordsMap count_words(const WordVector &words)
+std::map<std::string, std::size_t> count_words(const WordVector &words)
 {
-
+    std::map<std::string, std::size_t> tempMap{};
+    for (const auto &item : words)
+    {
+        tempMap[item]++;
+    }
+    return tempMap;
 }
 
-WordCountVec map_to_vector(const CountedWordsMap &counted_words)
+std::vector<std::pair<std::string, std::size_t>> map_to_vector(const std::map<std::string, std::size_t> &counted_words)
 {
+    std::vector<std::pair<std::string, std::size_t>> tempVecPair;
+    for (const auto &item : counted_words)
+    {
+        tempVecPair.push_back(item);
+    }
+    return tempVecPair;
+}
 
+//NOTE compare function is for display only, they doesn`t needed while use lambda-expression
+bool compare(const std::pair<std::string, std::size_t> &pair1, const std::pair<std::string, std::size_t> &pair2)
+{
+    return pair1.second > pair2.second;
 }
 
 void sort_word_counts(WordCountVec &word_counts)
 {
+    std::sort(word_counts.begin(), word_counts.end(), [](std::pair<std::string, std::size_t> &a, std::pair<std::string, std::size_t> &b)
+    {
+        return a.second > b.second;
+    });
 }
