a94fb042836ac3270cbdd5e8617efb213832870d
PvtTimaay <trip_tripper@gmx.net>
2024-03-04 22:46:53 +0100

    mssge

--- 08_STL/AlgorithmsLibrary/Exercise/lib.cc
+++ 08_STL/AlgorithmsLibrary/Exercise/lib.cc
@@ -10,10 +10,12 @@ namespace mystd
 
 bool equal(Iterator first1, Iterator last1, Iterator first2)
 {
+    return std::equal(first1, last1, first2);
 }
 
 Iterator fill_n(Iterator first, std::size_t count, const std::int32_t &value)
 {
+   return std::fill_n(first, count, value);
 }
 
 void iota(Iterator first, Iterator last, std::int32_t value)
