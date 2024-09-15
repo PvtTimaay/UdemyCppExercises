c1847449c19f5662f0069162574c62025571bce8
PvtTimaay <trip_tripper@gmx.net>
2024-09-15 17:09:42 +0200

    nochNichtFertig

--- 09_Objects/Shapes/Exercise/Shape.cc
+++ 09_Objects/Shapes/Exercise/Shape.cc
@@ -4,3 +4,15 @@
 
 #include "distance.hpp"
 #include "utils.hpp"
+
+double Shape::midpoint_distance(const Shape *other) const
+{
+    return get_distance(int a, int b, int c, int d);
+}
+
+Circle::Circle(int a, int b, int c)
+{
+    x_midpoint = a;
+    y_midpoint = b;
+    radius = c;
+}
--- 09_Objects/Shapes/Exercise/Shape.hpp
+++ 09_Objects/Shapes/Exercise/Shape.hpp
@@ -1,4 +1,40 @@
 #pragma once
 
-#include <cstdint>
-#include <utility>
+struct Shape
+{
+    Shape() = default;
+    ~Shape() noexcept = default;
+
+    virtual double get_area() const = 0;
+    double midpoint_distance(const Shape *other) const;
+    virtual std::pair<int, int> get_midpoint() const = 0;
+};
+
+struct Circle : Shape
+{
+    Circle() = default;
+    Circle(int a, int b, int c);
+    ~Circle() noexcept = default;
+
+    virtual double get_area() const final;
+    virtual std::pair <int, int> get_midpoint() const final;
+
+    int x_midpoint;
+    int y_midpoint;
+    int radius;
+};
+
+struct Rectangle : Shape
+{
+    Rectangle() = default;
+    Rectangle(int a, int b, int c, int d) : x1(a), y1(b), x2(c), y2(d){}
+    ~Rectangle() noexcept = default;
+
+    virtual double get_area() const final;
+    virtual std::pair <int, int> get_midpoint() const final;
+
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+};
